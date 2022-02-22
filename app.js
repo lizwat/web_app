1a492e6dd8251f069379f6b0ab67ecdf5ebc36bf
alexandrajaccard <alexandra.fong23@gmail.com>
2022-02-22 15:35:25 -0500

    added frontend for home, login, register, navbar, userprofile

--- app.js
+++ app.js
@@ -1,58 +1,67 @@
-const express               =  require('express'),
-      app                   =  express(),
-      mongoose              =  require("mongoose"),
-      passport              =  require("passport"),
-      bodyParser            =  require("body-parser"),
-      LocalStrategy         =  require("passport-local"),
-      passportLocalMongoose =  require("passport-local-mongoose"),
-      User                  =  require("./models/user");
+const express = require('express'),
+    app = express(),
+    mongoose = require("mongoose"),
+    passport = require("passport"),
+    bodyParser = require("body-parser"),
+    LocalStrategy = require("passport-local"),
+    passportLocalMongoose = require("passport-local-mongoose"),
+    User = require("./models/user");
 //Connecting database
 mongoose.connect("mongodb+srv://steve:chocolate3@cluster0.g7hvi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
 app.use(require("express-session")({
-    secret:"Any normal Word",       //decode or encode session
-    resave: false,          
-    saveUninitialized:false    
+    secret: "Any normal Word", //decode or encode session
+    resave: false,
+    saveUninitialized: false
 }));
-passport.serializeUser(User.serializeUser());       //session encoding
-passport.deserializeUser(User.deserializeUser());   //session decoding
+passport.serializeUser(User.serializeUser()); //session encoding
+passport.deserializeUser(User.deserializeUser()); //session decoding
 passport.use(new LocalStrategy(User.authenticate()));
-app.set("view engine","ejs");
-app.use(bodyParser.urlencoded(
-      { extended:true }
-))
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
 app.use(passport.initialize());
 app.use(passport.session());
 //=======================
 //      R O U T E S
 //=======================
-app.get("/", (req,res) =>{
+app.get("/", (req, res) => {
     res.render("home");
 })
-app.get("/userprofile",isLoggedIn ,(req,res) =>{
+app.get("/userprofile", isLoggedIn, (req, res) => {
     res.render("userprofile");
 })
 //Auth Routes
-app.get("/login",(req,res)=>{
+app.get("/login", (req, res) => {
     res.render("login");
 });
-app.post("/login",passport.authenticate("local",{
-    successRedirect:"/userprofile",
-    failureRedirect:"/login"
-}),function (req, res){
+
+app.get("/dashboard", (req, res) => {
+    res.render("dashboard");
 });
-app.get("/register",(req,res)=>{
+
+app.post("/login", passport.authenticate("local", {
+    successRedirect: "/dashboard",
+    failureRedirect: "/login"
+}), function (req, res) {});
+
+app.get("/register", (req, res) => {
     res.render("register");
 });
-app.post("/register",(req,res)=>{
-    
-    User.register(new User({username: req.body.username,phone:req.body.phone,telephone: req.body.telephone}),req.body.password,function(err,user){
-        if(err){
+app.post("/register", (req, res) => {
+
+    User.register(new User({
+        username: req.body.username,
+        phone: req.body.phone,
+        telephone: req.body.telephone
+    }), req.body.password, function (err, user) {
+        if (err) {
             console.log(err);
             res.render("register");
         }
-    passport.authenticate("local")(req,res,function(){
-        res.redirect("/login");
-    })    
+        passport.authenticate("local")(req, res, function () {
+            res.redirect("/login");
+        })
     })
 })
 
@@ -73,18 +82,19 @@ app.get("/logout",(req,res)=>{
     req.logout();
     res.redirect("/");
 });
-function isLoggedIn(req,res,next) {
-    if(req.isAuthenticated()){
+
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) {
         return next();
     }
     res.redirect("/login");
 }
 //Listen On Server
-app.listen(process.env.PORT ||3000,function (err) {
-    if(err){
+app.listen(process.env.PORT || 3000, function (err) {
+    if (err) {
         console.log(err);
-    }else {
+    } else {
         console.log("Server Started At Port 3000");
     }
-      
+
 });
